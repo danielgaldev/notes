ceaad36676667c4f6ce9f6f0a37b05e22b8e1166
Daniel Gal <daniel.gal@sch.bme.hu>
2019-11-20 23:30:50 +0100

    Create classes

--- api/src/semestr/urls.py
+++ api/src/semestr/urls.py
@@ -1,12 +1,15 @@
 from rest_framework_nested import routers
 from django.urls import path, include
 
-from .views import SemesterViewSet
+from .views import SemesterViewSet, ClassViewSet
 
 
 router = routers.SimpleRouter()
 router.register(r'semesters', SemesterViewSet, basename='semesters')
+semester_router = routers.NestedSimpleRouter(router, r'semesters', lookup='semester')
+semester_router.register(r'classes', ClassViewSet, base_name='semester-classes')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('', include(semester_router.urls))
 ]
--- api/src/semestr/views.py
+++ api/src/semestr/views.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
-from .models import Semester
-from .serializers import SemesterSerializer, SemesterDetailSerializer
+from .models import Semester, Class
+from .serializers import SemesterSerializer, SemesterDetailSerializer, ClassSerializer
 
 
 class SemesterViewSet(ListModelMixin, RetrieveModelMixin, viewsets.GenericViewSet):
@@ -17,3 +19,14 @@ class SemesterViewSet(ListModelMixin, RetrieveModelMixin, viewsets.GenericViewSe
         if self.action == 'retrieve':
             return SemesterDetailSerializer
         return SemesterSerializer
+
+
+class ClassViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+        
+    def create(self, request, pk=None, semester_pk=None):
+        name = request.data['name']
+        user = request.user
+        semester = get_object_or_404(Semester, user=user, pk=semester_pk)
+        class_object = Class.objects.create(name=name, parent_semester=semester)
+        return Response(ClassSerializer(class_object).data, status=status.HTTP_201_CREATED)
