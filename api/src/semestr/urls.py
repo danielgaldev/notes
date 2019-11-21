0338327709e7106151e29f5e3fc7c4e47ebef0bb
Daniel Gal <daniel.gal@sch.bme.hu>
2019-11-21 01:18:57 +0100

    Create & delete requirements

--- api/src/semestr/urls.py
+++ api/src/semestr/urls.py
@@ -1,15 +1,19 @@
 from rest_framework_nested import routers
 from django.urls import path, include
 
-from .views import SemesterViewSet, ClassViewSet
+from .views import SemesterViewSet, ClassViewSet, RequirementViewSet
 
 
 router = routers.SimpleRouter()
 router.register(r'semesters', SemesterViewSet, basename='semesters')
 semester_router = routers.NestedSimpleRouter(router, r'semesters', lookup='semester')
 semester_router.register(r'classes', ClassViewSet, base_name='semester-classes')
+class_router = routers.NestedSimpleRouter(semester_router, r'classes', lookup='class')
+class_router.register(r'requirements', RequirementViewSet, base_name='class-requirements')
+
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(semester_router.urls))
+    path('', include(semester_router.urls)),
+    path('', include(class_router.urls))
 ]
--- api/src/semestr/views.py
+++ api/src/semestr/views.py
@@ -4,8 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
-from .models import Semester, Class
-from .serializers import SemesterSerializer, SemesterDetailSerializer, ClassSerializer
+from .models import Semester, Class, Requirement
+from .serializers import SemesterSerializer, SemesterDetailSerializer, ClassSerializer, RequirementSerializer
 
 
 class SemesterViewSet(ListModelMixin, RetrieveModelMixin, viewsets.GenericViewSet):
@@ -32,7 +32,33 @@ class ClassViewSet(DestroyModelMixin, viewsets.ViewSet):
         return Response(ClassSerializer(class_object).data, status=status.HTTP_201_CREATED)
 
     def destroy(self, request, pk=None, semester_pk=None):
-        queryset = Class.objects.filter(pk=pk, parent_semester=semester_pk)
+        user = request.user
+        queryset = Class.objects.filter(parent_semester=semester_pk, parent_semester__user=user)
+        obj = get_object_or_404(queryset, pk=pk)
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RequirementViewSet(DestroyModelMixin, viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+        
+    def create(self, request, pk=None, semester_pk=None, class_pk=None):
+        text = request.data['text']
+        class_object = get_object_or_404(
+            Class, 
+            parent_semester=semester_pk, 
+            parent_semester__user=request.user, 
+            pk=class_pk
+        )
+        requirement = Requirement.objects.create(text=text, parent_class=class_object, done=False)
+        return Response(RequirementSerializer(requirement).data, status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, pk=None, semester_pk=None, class_pk=None):
+        queryset = Requirement.objects.filter(
+            parent_class=class_pk, 
+            parent_class__parent_semester=semester_pk,
+            parent_class__parent_semester__user=request.user
+        )
         obj = get_object_or_404(queryset, pk=pk)
         obj.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
