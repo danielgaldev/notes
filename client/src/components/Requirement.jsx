e1aa129eef0974895aaf581a319f23ab19b8f14e
Daniel Gal <daniel.gal@sch.bme.hu>
2019-11-28 23:25:25 +0100

    Another small refactor

--- client/src/components/Requirement.jsx
+++ client/src/components/Requirement.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import useAxios from 'axios-hooks';
+//
+import { onSuccess } from '../utils/helpers';
 
 
 export default function Requirement({ requirement, classId, semesterId, update }) {
   const [done, setDone] = React.useState(requirement.done);
-  function validateStatus(status) {
-    if (status >= 200 && status < 300) {
-      update();
-      return true;
-    } else {
-      return false;
-    }
-  }
   const [, executeDelete] = useAxios({
     url: `/api/v1/semesters/${semesterId}/classes/${classId}/requirements/${requirement.id}/`,
     method: 'DELETE',
-    validateStatus
+    validateStatus: onSuccess(() => update())
   }, { manual: true });
-  
+
   function handleChange(e) {
     console.log(`PATCH /api/v1/semesters/${semesterId}/classes/${classId}/requirements/${requirement.id}/ { done: ${!done} }`);
     setDone(!done);
   };
-  
+
   return (
     <li>
       <label>
