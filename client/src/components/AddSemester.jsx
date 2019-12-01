a1432fca8c727a723cfeca997192f085e24d70a1
Daniel Gal <daniel.gal@sch.bme.hu>
2019-12-01 15:06:29 +0100

    react-icons

--- client/src/components/AddSemester.jsx
+++ client/src/components/AddSemester.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useAxios from 'axios-hooks';
+import { FaBeer } from 'react-icons/fa';
 //
 import { onSuccess } from '../utils/helpers';
 
@@ -22,7 +23,7 @@ export default function AddSemester({ update }) {
   return (
     <form onSubmit={handleSubmit}>
       <input type='text' placeholder='Semester number' value={number} onChange={e => setNumber(e.target.value)} />
-      <button type='submit' className='bg-purple-500 hover:bg-purple-600 p-2'>Add</button>
+      <button type='submit' className='bg-purple-500 hover:bg-purple-600 p-2'>Add <FaBeer className='text-white text-4xl' /></button>
     </form>
-  );
-}
+      );
+    }
