8a10f18889483003e7597cc372a256e624dd309e
Daniel Gal <daniel.gal@sch.bme.hu>
2019-12-02 21:15:46 +0100

    Go back link

--- client/src/pages/SemesterDetailPage.jsx
+++ client/src/pages/SemesterDetailPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useAxios from 'axios-hooks';
+import { Link } from 'react-router-dom';
+import { FaChevronLeft } from 'react-icons/fa';
 //
 import AddClass from '../components/AddClass';
 import ClassListItem from '../components/ClassListItem';
@@ -17,7 +19,14 @@ export default function SemesterDetailPage() {
 
   return (
     <main>
-      {data && <h2>{data.number}. semester</h2>}
+      <div className='w-32 p-4'>
+        <Link to='/'>
+          <span className='flex items-center'>
+            <FaChevronLeft />
+            <span className='ml-2 hover:underline'>Go back</span>
+          </span>
+        </Link>
+      </div>
       {data && <ul className='p-3 md:col-count-2 lg:col-count-3 xl:col-count-4 xl:max-w-8xl'>
         <AddClass semesterId={id} update={update} />
         {data.classes.map(c => <ClassListItem key={c.id} clas={c} semesterId={id} update={update} />)}
