18baab473da7a57b821b7d68ebfb170477405cb7
Daniel Gal <daniel.gal@sch.bme.hu>
2019-12-08 17:47:19 +0100

    Template list, create, delete

---
+++ client/src/components/AdminTemplateWrapper.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+export default function AdminTemplateWrapper({ template, deleteTemplate }) {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <li className='border border-solid border-grey-300 p-2 mt-3'>
+      <div className='w-full flex justify-between items-center'>
+        <span className='text-2xl ml-2'>{template.name}</span>
+        <div>
+          <button
+            onClick={() => setOpen(!open)}
+            className='p-2 underline hover:no-underline'>
+            {open ? 'Hide' : 'View'}
+          </button>
+          <button
+            onClick={() => deleteTemplate(template.id)}
+            className='p-2 underline hover:no-underline'>
+            Delete
+          </button>
+        </div>
+      </div>
+      {open && <div>hey</div>}
+    </li>
+  )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import useAxios from 'axios-hooks';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 //
 import AddSemester from '../components/AddSemester';
 import SemesterListItem from '../components/SemesterListItem';
 
 
-function Home() {
+function Home({ isAdmin }) {
   const [{ data }, update] = useAxios('/api/v1/semesters');
   React.useEffect(() => {
     update();
   }, [update]);
 
   return (
-    <main className='p-4'>
-      {data && <ul className='flex flex-row flex-wrap'>
+    <main>
+      {isAdmin && <nav className='w-full p-4 flex justify-end'>
+        <Link to='/admin'>
+          <span className='flex items-center'>
+            <span className='ml-2 underline hover:no-underline'>Admin page</span>
+          </span>
+        </Link>
+      </nav>}
+
+      {data && <ul className='flex flex-row flex-wrap mt-3'>
         {data.map(s => <SemesterListItem key={s.id} semester={s} update={update} />)}
         <AddSemester update={update} />
       </ul>}
@@ -21,4 +31,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default connect(state => ({ isAdmin: state.auth.user.is_staff }))(Home);
